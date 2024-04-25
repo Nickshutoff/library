a23ef9f8c0ca86937d75b1cbfed7bd007d37b879
Nikita Shutov <nickshutoff@gmail.com>
2024-04-25 17:09:36 +0300

    34

--- script.js
+++ script.js
@@ -24,14 +24,33 @@ mobileMenuLinks.forEach(link => {
 // About
 const carouselBtn = document.querySelectorAll('.carousel-btn')
 const carousel = document.querySelector('.carousel-content')
+const carouselSwipeLeft = document.querySelector('.carousel-swipe-left')
+const carouselSwipeRight = document.querySelector('.carousel-swipe-right')
+
 
 carouselBtn.forEach((btn, index) => {
     btn.addEventListener('click', () => {
         carouselBtn.forEach(el => {
             el.classList.remove('active')
         })
-        
+        btnIndex = index
         btn.classList.add('active')
         carousel.style.transform = `translateX(-${index * 475}px)`
     })
+})
+
+let btnIndex = 0
+
+carouselSwipeRight.addEventListener('click', () => {
+    if (btnIndex < carouselBtn.length - 1) {
+        btnIndex++
+        carouselBtn[btnIndex].click()
+    }
+})
+
+carouselSwipeLeft.addEventListener('click', () => {
+    if (btnIndex > 0) {
+        btnIndex--
+        carouselBtn[btnIndex].click()
+    }
 })
